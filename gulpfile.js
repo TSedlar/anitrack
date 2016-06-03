52dae91db71c382ec2e4a3659595dcca097eb305
Tyler Sedlar <sedlarizona@gmail.com>
2016-06-03 06:31:57 -0700

    Remove lib dir and add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp')
+var source = require('vinyl-source-stream')
+var browserify = require('browserify')
+var babel = require('babelify')
+var glob = require('glob')
+var path = require('path')
+var del = require('del')
+
+var OUT_DIR = './lib'
+
+function bundle (indexFile, dir, deps) {
+  var bundler = browserify(indexFile, { debug: true }).transform(babel)
+  bundler.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(dir))
+  for (var key in deps) {
+    gulp.src(glob.sync(key))
+      .pipe(gulp.dest(path.join(dir, deps[key])))
+  }
+}
+
+gulp.task('clean', function () {
+  del(OUT_DIR)
+})
+
+gulp.task('build', function () {
+  bundle('./src/index.js', OUT_DIR, {
+    './plugin/*': '',
+    './plugin/images/*': 'images/'
+  })
+})
+
+gulp.task('default', ['clean', 'build'])
